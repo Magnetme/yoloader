b5d7ed07a2087bdb290d114445ff6147d2c78ddf
Tiddo Langerak <tiddolangerak@gmail.com>
2015-02-20 12:58:30 +0100

    Fixed __filname in dirname plugin

--- src/plugins/dirname.js
+++ src/plugins/dirname.js
@@ -7,7 +7,7 @@ module.exports = through.obj.bind(through, (chunk, enc, done) => {
 	let fileContent = chunk.contents.toString();
 	//Stringify should automatically make sure that the string that comes out of it is quoted
 	//and valid javascript
-	let filename = JSON.stringify(path.basename(chunk.path));
+	let filename = JSON.stringify(path.relative(chunk.base, chunk.path));
 	let dirname = JSON.stringify('/' + path.relative(chunk.base, path.dirname(chunk.path)));
 	let prefix = `var __dirname=${dirname},__filename=${filename};`;
 	chunk.contents = new Buffer(prefix + fileContent);
