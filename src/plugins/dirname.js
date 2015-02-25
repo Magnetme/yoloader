dbaf3b24e929e3ff427a68d72a98b16e4b1ec6fb
Tiddo Langerak <tiddolangerak@gmail.com>
2015-02-25 20:58:18 +0100

    Allow base path for dirname plugin
    Also fixes inconsistency between filename and dirname: dirname was
    prefixed with a slash, filename wasn't.

--- src/plugins/dirname.js
+++ src/plugins/dirname.js
@@ -3,17 +3,23 @@ let path = require('path');
 let applySourceMap = require('vinyl-sourcemaps-apply');
 let prefixSourceMap = require('./util/prefixSourceMap');
 
-module.exports = through.obj.bind(through, (chunk, enc, done) => {
-	let fileContent = chunk.contents.toString();
-	//Stringify should automatically make sure that the string that comes out of it is quoted
-	//and valid javascript
-	let filename = JSON.stringify(path.relative(chunk.base, chunk.path));
-	let dirname = JSON.stringify('/' + path.relative(chunk.base, path.dirname(chunk.path)));
-	let prefix = `var __dirname=${dirname},__filename=${filename};`;
-	chunk.contents = new Buffer(prefix + fileContent);
-	if (chunk.sourceMap) {
-		let map = prefixSourceMap(chunk, prefix, fileContent);
-		applySourceMap(chunk, map);
-	}
-	done(null, chunk);
-});
+module.exports = function(base) {
+	base = base || '/';
+	return through.obj((chunk, enc, done) => {
+		let fileContent = chunk.contents.toString();
+		let filePath = path.relative(chunk.base, chunk.path);
+		filePath = path.resolve(base, filePath);
+		let dirPath = path.dirname(filePath);
+		//Stringify should automatically make sure that the string that comes out of it is quoted
+		//and valid javascript
+		let filename = JSON.stringify(filePath);
+		let dirname = JSON.stringify(dirPath);
+		let prefix = `var __dirname=${dirname},__filename=${filename};`;
+		chunk.contents = new Buffer(prefix + fileContent);
+		if (chunk.sourceMap) {
+			let map = prefixSourceMap(chunk, prefix, fileContent);
+			applySourceMap(chunk, map);
+		}
+		done(null, chunk);
+	});
+};
