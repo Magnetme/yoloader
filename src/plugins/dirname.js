2349100a411041416791febaa56abee60faeebf9
Tiddo Langerak <tiddolangerak@gmail.com>
2015-02-25 21:02:45 +0100

    Allow both base and prefix to be set for dirname plugin

--- src/plugins/dirname.js
+++ src/plugins/dirname.js
@@ -3,12 +3,20 @@ let path = require('path');
 let applySourceMap = require('vinyl-sourcemaps-apply');
 let prefixSourceMap = require('./util/prefixSourceMap');
 
-module.exports = function(base) {
-	base = base || '/';
+/**
+ * Creates the __dirname and __filename variables for a file.
+ *
+ * @param {String} [opts.base] Base path from which __dirname and __filename will be calculated.
+ *                             If no base path is given chunk.base will be used.
+ * @param {String} [opts.prefix] path that will be prefixed to __dirname and __filename.
+ */
+module.exports = function(opts) {
+	let pathPrefix = opts.prefix || '/';
 	return through.obj((chunk, enc, done) => {
+		let base = opts.base || chunk.base;
 		let fileContent = chunk.contents.toString();
-		let filePath = path.relative(chunk.base, chunk.path);
-		filePath = path.resolve(base, filePath);
+		let filePath = path.relative(base, chunk.path);
+		filePath = path.resolve(pathPrefix, filePath);
 		let dirPath = path.dirname(filePath);
 		//Stringify should automatically make sure that the string that comes out of it is quoted
 		//and valid javascript
