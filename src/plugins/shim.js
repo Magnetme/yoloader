7f4d5fb3dba4b91264177ea130a71d34a73edfd7
Tiddo Langerak <tiddolangerak@gmail.com>
2015-06-01 16:14:34 +0200

    Fixed finding of mainFile for shim

--- src/plugins/shim.js
+++ src/plugins/shim.js
@@ -79,9 +79,11 @@ function findShim(file, shims, packageShims, cb) {
 					isMain = mainFile.reduce((previous, current) => {
 						return current && path.join(path.dirname(res.path), current) === file;
 					}, false);
-				} else if (!mainFile || typeof mainFile !== 'string') {
-					//Default value
-					mainFile = 'index.js';
+				} else {
+					if (!mainFile || typeof mainFile !== 'string') {
+						//Default value
+						mainFile = 'index.js';
+					}
 					isMain = path.join(path.dirname(res.path), mainFile) === file;
 				}
 				let shim = isMain && packageShims[res.pack.name];
